bb8b64af132c686b95067978ea0507d9d1616804
Tushar Jain <tushar00jain@users.noreply.github.com>
2026-06-20 14:09:57 +0000

    vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import MahjongAdvisor from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <MahjongAdvisor />
+    <Analytics />
+    <SpeedInsights />
   </React.StrictMode>
 );
